c316e7595fbbade8429d6d4e1b6c9b355208db5e
mfalden <malden2023@chadwickschool.org>
2022-01-26 13:05:26 -0800

    implement method stubs

--- Program.cs
+++ Program.cs
@@ -1,12 +1,88 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace malden_Personal_Project_1
 {
-    class Program
+    class HighScore
     {
-        static void Main(string[] args)
+        
+        /// <summary>
+        /// Loads the "scoresfile.txt" file and stores it in list "ScoreList". 
+        /// </summary>
+        /// <param name="scoresFile">a .txt file storing all scores.</param>
+        /// <returns>Returns "ScoreList".</returns>
+        static List<string> LoadScoresFile(string scoresFile)
         {
-            Console.WriteLine("Hello World!");
+                // 1. Create list scoreList
+                // 2. scorelist = file.ReadLines(scoresfile.txt);
+                // 3. return list scorelist
+        return null;
+        }
+
+        /// <summary>
+        /// Loads the list "ScoreList" and takes the score values only.
+        /// </summary>
+        /// <param name="scoreList">A list of all usernames and their scores</param>
+        /// <returns>Returns int list "ScoresOnly".</returns>
+        static List<int> ScoreSplit(List<string> scoreList)
+        {
+                // 1. Split the scoreList along all spaces (" ")
+                // 2. create new list<int> = scoresOnly
+                // 3. add element 2 using int.Parse(scoresOnly[1]);
+                // 4. return list<int> scoresOnly;
+            return null;
+        }
+
+        /// <summary>
+        /// prompts the user's name and score values and stores them in two strings, "userScore" and "userName".
+        /// </summary>
+        /// <returns>The function returns userScore and userName.</returns>
+        static (int, string) UserScore()
+        {
+                // 1. create string userName
+                // 2. create int userScore
+                // 3. display "please type in your name"
+                // 4. collect user input
+                // 5. trim user input
+                // 6. add to string userName
+                // 7. start of loop: display "please type in your score"
+                // 8. collect user input
+                // 9. trim user input
+                // 10. if the score includes letters, display "invalid score" and restart the loop. If the score is only numbers, add the user input to integer userScore
+                // 11. Return int userScore, string userName
+
+            return (-1, null);
+        }
+        /// <summary>
+        /// Takes the string "userscore" and compares it to the values in scoresOnly, stopping only when the userScore is greater than the value in an index of scoresOnly.
+        /// </summary>
+        /// <param name="scoresOnly">A list containing the scores of past players</param>
+        /// <param name="userScore">The user's score</param>
+        /// <returns>returns the index number of the row where userScore was greater than scoresOnly in an integer "insertAt".</returns>
+        static int ScoreCompare(List<int> scoresOnly, string userScore)
+        {
+                // 1. load list<int> scoresOnly and int userScore
+                // 2. create new int inserAt and set to 0
+                // 3. start of loop: for each line in scoresOnly--
+                // 4. if the user score is less than scoresOnly, increase int insertAt by one and restart the loop
+                // 5. if the user score is greater than scores only, return int insertAt
+            return -1;
+        }
+
+        /// <summary>
+        /// Takes the "userName" and "userScore" strings, combines them into a string entry and inserts them into the list "scorelist" at the index specified by int "insertAt". It then displays all scores.
+        /// </summary>
+        /// <param name="userName"></param>
+        /// <param name="userScore"></param>
+        /// <param name="insertAt"></param>
+        /// <param name="scoreList"></param>
+        static void AddScore(string userName, string userScore, int insertAt, List<int> scoreList)
+        {
+                // 1. Load in the userName, userScore, insertAt, and scoreList variables. 
+                // 2. Create String "entry" $"{userName} {userScore}"
+                // 3. Insert "entry" at index "insertAt" 
+                // 4. Using WriteLine, Display list scoreList
+                // 5. Using File.WriteLines, override all entries in scoresFile.txt to be entries in list scoreList
         }
     }
 }
